cc08f05708f63667466d9ddd5fdef1f35200dd6d
Shadowempty71 <shadowempty71@gmail.com>
2024-07-21 21:26:14 +0300

    Sea Battle 1

--- Изменяемые.py
+++ Изменяемые.py
@@ -1,10 +1,19 @@
-def D(a,b,c):
-    return b**2 - 4*a*c
+# field =[['-'] * 3 for i in range(3)] # игровое поле
+# field[2][2] = 'X'
+#
+# def conclusion():# функция вывода в консоль
+#     print(f"  0 1 2")
+#     print(f"0 {field[0][0]} {field[0][1]} {field[0][2]}")
+#     print(f"1 {field[1][0]} {field[1][1]} {field[1][2]}")
+#     print(f"2 {field[2][0]} {field[2][1]} {field[2][2]}")
+# conclusion()
 
-def quadratic_solve(a,b,c)
-    if D(a,b,c) < 0:
-        return "Нет вещественных корней"
-    elif D(a,b,c) == 0:
-        return -x = -b/(2*a)
+def move():
+    x, y = map(int, input("Ваш ход. Введите числа через пробел:").split())
+    if any((2 < item) or (item < 0) for item in(x, y)):
+        print("Такой ход недопустим")
+
     else:
-        return (-b - D(a, b, c) ** 0.5) / (2 * a), (-b + D(a, b, c) ** 0.5) / (2 * a)
+        return x, y
+
+move()
